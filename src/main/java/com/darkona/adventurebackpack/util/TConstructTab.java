b224039a119f69c7c6e3259818a14ecb7cb6784d
ghostflyby <ghostflyby+git@outlook.com>
2023-09-28 00:27:13 +0800

    Fix: opening GUI and container

--- src/main/java/com/darkona/adventurebackpack/util/TConstructTab.java
+++ src/main/java/com/darkona/adventurebackpack/util/TConstructTab.java
@@ -3,34 +3,27 @@ package com.darkona.adventurebackpack.util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 
-import com.darkona.adventurebackpack.AdventureBackpack;
-import com.darkona.adventurebackpack.handlers.GuiHandler;
+import com.darkona.adventurebackpack.init.ModNetwork;
+import com.darkona.adventurebackpack.network.GUIPacket;
+import com.darkona.adventurebackpack.reference.BackpackTypes;
 
-import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
 import tconstruct.client.tabs.AbstractTab;
 
 public class TConstructTab extends AbstractTab {
 
     public TConstructTab() {
-        super(0, 0, 0, Wearing.getWearingWearable(Minecraft.getMinecraft().thePlayer));
+        super(5, 0, 0, BackpackUtils.createBackpackStack(BackpackTypes.STANDARD));
     }
 
     @Override
     public void onTabClicked() {
         EntityClientPlayerMP playerMP = Minecraft.getMinecraft().thePlayer;
-        int backpackType = 0;
-        if (Wearing.getWearingBackpack(playerMP) != null) backpackType = GuiHandler.BACKPACK_WEARING;
-        else if (Wearing.getWearingCopter(playerMP) != null) backpackType = GuiHandler.COPTER_WEARING;
-        else if (Wearing.getWearingJetpack(playerMP) != null) backpackType = GuiHandler.JETPACK_WEARING;
+        byte backpackType = 0;
+        if (Wearing.getWearingBackpack(playerMP) != null) backpackType = GUIPacket.BACKPACK_GUI;
+        else if (Wearing.getWearingCopter(playerMP) != null) backpackType = GUIPacket.COPTER_GUI;
+        else if (Wearing.getWearingJetpack(playerMP) != null) backpackType = GUIPacket.JETPACK_GUI;
         else return;
-        FMLNetworkHandler.openGui(
-                playerMP,
-                AdventureBackpack.instance,
-                backpackType,
-                playerMP.getEntityWorld(),
-                (int) playerMP.posX,
-                (int) playerMP.posY,
-                (int) playerMP.posZ);
+        ModNetwork.net.sendToServer(new GUIPacket.GUImessage(backpackType, GUIPacket.FROM_WEARING));
 
     }
 
